17990edc911b91fddb4bdc73071e678c424358a6
rsps950551 <rsps950551@gmail.com>
2016-11-24 20:13:37 +0800

    ff

--- main.go
+++ main.go
@@ -61,8 +61,6 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	// GG
-	httpGet()
 	//GG
 	for _, event := range events {
 		if event.Type == linebot.EventTypeMessage {
