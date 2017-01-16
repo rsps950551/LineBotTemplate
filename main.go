d73652f5bfeef4a20efa68f41dbc987a6a03add1
rsps950551 <rsps950551@gmail.com>
2017-01-16 11:17:39 +0800

    FF

--- main.go
+++ main.go
@@ -212,12 +212,12 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 				// mysql()
 				//message.ID+":"+message.Text
         if(echo == "bottun"){
-            if _, err = bot.ReplyMessage(event.ReplyToken, templatemessgage).Do(); err != nil {
-            log.Print(err) }
+          if _, err = bot.ReplyMessage(event.ReplyToken, templatemessgage).Do()
+        } else {
+          if _, err = bot.ReplyMessage(event.ReplyToken, echo).Do()
         }
-				else{
-          if _, err = bot.ReplyMessage(event.ReplyToken, echo).Do(); err != nil {
-            log.Print(err) }
+				
+          
         
 			}
 		}
