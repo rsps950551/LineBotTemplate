92477dd28993ef4dc5d2e7341724e2ed928bb471
rsps950551 <rsps950551@gmail.com>
2017-01-20 17:15:20 +0800

    GG

--- main.go
+++ main.go
@@ -189,7 +189,7 @@ func httpGet(q string) {
     body, err := ioutil.ReadAll(resp.Body)
     // er := json.NewDecoder(strings.NewReader(body)).Decode(ff)
     json.Unmarshal(body, &u)
-    echo = u["entity"].([]string)[0]
+    echo = u["entity"].(string)
 
     if(q=="give me bottun"){
       echo = "bottun"
