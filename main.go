43c7d241adf4eda1e845bcd098a21b55d8a03cd7
rsps950551 <rsps950551@gmail.com>
2017-02-09 20:13:04 +0800

    FF

--- main.go
+++ main.go
@@ -222,7 +222,7 @@ func httpGet(q string) {
                                    echo := echo+"是 陣列："
                                    for i, iv := range v2 {
                                           fmt.Println(i, "：", iv)
-                                     echo := echo+"Key："+(string)i+"Value："+iv.(string)
+                                     echo := echo+"Value："+iv.(string)
                                    }
                             default:
                                    fmt.Println(k, " 是其他型別...")
