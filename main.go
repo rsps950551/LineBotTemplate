5e589511c64b276dd5d2ac37ce727c680158b835
rsps950551 <rsps950551@gmail.com>
2017-02-10 16:34:51 +0800

    FF

--- main.go
+++ main.go
@@ -207,7 +207,7 @@ func httpGet(q string) {
     //   echo = echo + n + a
     // }  
     //json.Unmarshal(, &resultContent)
-    echo = r["resultContent"].Index(0)
+    echo = r["resultContent"]["entity"]
 
     if(q=="give me bottun"){
       echo = "bottun"
