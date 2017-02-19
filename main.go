a42b197cfdecb88d68740dab1a46d4f1fde30970
rsps950551 <rsps950551@gmail.com>
2017-02-19 21:03:13 +0800

    FF

--- main.go
+++ main.go
@@ -258,6 +258,8 @@ func httpGet(q string) {
         for e:= entity.Front();e!=nil;e = e.Next(){
          op += e.Value.(string)+" "
         }
+      } else {
+        OP = ""
       }
     }
     //test~~~~~~
@@ -277,6 +279,7 @@ func httpGet(q string) {
 
 func callbackHandler(w http.ResponseWriter, r *http.Request) {
 	events, err := bot.ParseRequest(r)
+
   leftBtn := linebot.NewMessageTemplateAction("練習題", "我要練習題 "+op)
   rightBtn := linebot.NewMessageTemplateAction("教材", "我要教材 "+op)
 
@@ -304,10 +307,10 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
         if bottun {
            // _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(echo)).Do()
            _, err = bot.ReplyMessage(event.ReplyToken, templatemessgage).Do()
-           
+
         } else {
            _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(echo)).Do()
-           op = ""
+           
         }
 				
           
