ec205d978f6b05fbf01f80b47a4220db29e0fd06
rsps950551 <rsps950551@gmail.com>
2017-06-20 20:43:19 +0800

    FF

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+  "unicode/utf8"
   //"container/list"
   //"bytes"
 	"net/http"
@@ -190,7 +191,7 @@ func httpGet(q string) {
     echo = "OK"
     bottun = false
     op = ""
-    resp, err := http.Get("http://140.115.54.93:54321/chatbot?q="+q)
+    resp, err := http.Get("http://140.115.54.93:54321/chatbot?q="+utf8.DecodeLastRuneInString(q))
     if err != nil {
         // handle error
        panic(err.Error())
