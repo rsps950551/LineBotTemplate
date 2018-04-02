940aec6d8a01bb6d6c90d077e8b89178b3f54cb4
rsps950551 <rsps950551@gmail.com>
2018-04-02 22:58:44 +0800

    ff

--- main.go
+++ main.go
@@ -60,8 +60,7 @@ func main() {
 	http.ListenAndServe(addr, nil)	
 
     http.HandleFunc("/", sayhelloName) // set router
-    addr := fmt.Sprintf(":%s", 9090)
-    err := http.ListenAndServe(addr, nil) // set listen port
+    err := http.ListenAndServe(":9090", nil) // set listen port
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
     }
